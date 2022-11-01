a80cc8cc096cddf1e888d7f12940ee763a9dce33
pcuestamorer <pcuestamorer@gmail.com>
2022-11-01 11:33:35 +0100

    init exam-1-21-22

--- exam-I-21-22/src/test/java/main/Main.java
+++ exam-I-21-22/src/test/java/main/Main.java
@@ -1,18 +1,9 @@
 package main;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 /**
  * @author pablo
  *
  */
 class Main {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }

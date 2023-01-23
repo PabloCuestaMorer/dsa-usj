9e7289e74920e5412432c163d8d85764557fefce
pcuestamorer <pablocuestam@outlook.com>
2023-01-23 08:14:10 +0100

    test egit

--- SortingAlgorithms/src/beans/InsertionSort.java
+++ SortingAlgorithms/src/beans/InsertionSort.java
@@ -12,9 +12,10 @@ public class InsertionSort {
 			// previous element
 			int j = i - 1;
 
-			/* Move elements of arr[0..i-1], that are
-            greater than key (left side of the key), 
-            to one position ahead of their current position. */
+			/*
+			 * Move elements of arr[0..i-1], that are greater than key (left side of the
+			 * key), to one position ahead of their current position.
+			 */
 			while (j >= 0 && arr[j] > key) {
 				arr[j + 1] = arr[j];
 				j--;
